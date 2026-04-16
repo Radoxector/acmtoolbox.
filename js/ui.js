f588a47be528d216389689e0c8e23e0b1c81f7ab
Radoxector <h24111@gmail.com>
2026-04-16 02:11:21 +0100

    deleted

--- js/ui.js
+++ js/ui.js
@@ -72,7 +72,7 @@ function renderSVG(result) {
   const viewBox = `${minX - padding} ${minY - padding} ${w + padding * 2} ${h + padding * 2}`;
 
   let svg = `<svg width="${w + padding * 2}mm" height="${h + padding * 2}mm" viewBox="${viewBox}" xmlns="http://www.w3.org/2000/svg">`;
-  svg += `<rect width="100%" height="100%" fill="white"/>`;
+  
 
   // Seams layer
   svg += `<g id="flat_seams">`;
